7b1594c45b9b0d65a30e59acca710f31d6eef95f
saurabh <50881320+SauRaj-Code@users.noreply.github.com>
2021-10-30 11:03:01 +0530

    Add files via upload

--- program3/program3.c
+++ program3/program3.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+void main()
+{
+	//Enrolment Number
+	printf("-----------------\n");
+	printf("|202103103510362|\n");
+	printf("-----------------\n");
+
+	//Program
+	int num1,num2,sum;
+	printf("Enter number1: ");
+	scanf("%d",&num1);
+	printf("Enter number2: ");
+	scanf("%d",&num2);
+	sum = num1 + num2;
+	printf("The addition of %d and %d is: %d\n",num1,num2,sum);
+}
