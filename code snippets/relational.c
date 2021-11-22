b71ba4ea53b2afee40b18165eb0299cc0e9810cc
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2021-11-22 12:57:54 +0530

    Making Code Snippet and Adding In IT.

---
+++ code snippets/relational.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    /* code */
+    int num1 = 5,num2 = 4;
+    printf("5 > 4 = %d\n",num1>num2);
+    printf("5 < 4 = %d\n",num1<num2);
+    printf("5 <= 4 = %d\n",num1<=num2);
+    printf("5 >= 4 = %d\n",num1>=num2);
+    printf("5 == 4 = %d\n",num1==num2);
+    printf("5 != 4 = %d\n",num1!=num2);
+    return 0;
+}
