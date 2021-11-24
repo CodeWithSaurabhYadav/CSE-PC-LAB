933bf6e146ded75a83198277f87afef57b42e6c7
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2021-11-24 14:34:45 +0530

    Uploading new files for operators
    Conditional and increment and decrement operators

---
+++ code snippets/Conditional_Operator.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main()
+{
+    int age;
+    printf("Enter your age : ");scanf("%d",&age);
+    (age>=18)?printf("You are eligible for Voting\n"):printf("You are not eligible for Voting\n");
+}
