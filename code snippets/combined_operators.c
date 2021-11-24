62cfffbfae0439d90f69798849718a3131e146b4
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2021-11-24 14:08:14 +0530

    Uploading Combined_Operator

---
+++ code snippets/combined_operators.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+    /* code */
+    int num = 362;
+    printf("%d\n",num+=2);
+    printf("%d\n",num-=2);
+    printf("%d\n",num*=2);
+    printf("%d\n",num/=2);
+    printf("%d\n",num&=2);
+    printf("%d\n",num|=2);
+    printf("%d\n",num^=2);
+    printf("%d\n",num>>=2);
+    printf("%d\n",num<<=2);
+}
