2ef26ddf406348c029e1306f2a38713c55cd6065
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2021-12-24 15:07:03 +0530

    Add files via upload

--- program21/program21.c
+++ program21/program21.c
@@ -4,8 +4,9 @@
 int main()
 {
     /* initial code */
-    int a[5] = {1,45,3,6,5};
+    int a[5] = {2,45,1,6,5};
     int max = a[0];
+    // Largest
     for (int i = 1; i < 5; i++)
     {
         /* cheacking the condition */
@@ -16,5 +17,19 @@ int main()
         }
     }
     printf("The largest number is : %d\n",max);
+    // Smallest
+    int min = a[0];
+    for (int j = 1; j < 5; j++)
+    {
+        /* code */
+        if (min > a[j])
+        {
+            /* code */
+            min = a[j];
+        }
+    }
+    printf("The smallest number is : %d\n",min);
+    
+
     return 0;
 }
