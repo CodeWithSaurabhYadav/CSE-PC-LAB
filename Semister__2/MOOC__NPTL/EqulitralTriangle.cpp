5bd80ef59dac240a40610171d9f8a4c30a2c28f6
saurabh <sauraj.contact@gmail.com>
2022-02-06 21:09:20 +0530

    save

--- Semister__2/MOOC__NPTL/EqulitralTriangle.cpp
+++ Semister__2/MOOC__NPTL/EqulitralTriangle.cpp
@@ -3,23 +3,10 @@
 main_program{
     turtleSim();
 
-    int base = 100;
-    int side = 10;
-
-    repeat(24){
-        repeat(4){
-            forward(side*2);
-            left(90);
-        }
-        repeat(3){
-            forward(base);
-            left(120);
-        }
-        repeat(3){
-            forward(base*2);
-            left(120);
-        }
-        left(15);
+    int side = 100;
+    repeat(4){
+        forward(side);
+        left(120);
     }
     wait(20);
 }
