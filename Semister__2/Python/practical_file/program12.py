a0944bcb55b0de628e99f41e97cee162cec5059e
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2022-05-10 14:46:50 +0530

    Add files via upload

--- Semister__2/Python/practical_file/program12.py
+++ Semister__2/Python/practical_file/program12.py
@@ -7,3 +7,22 @@ attributes and a method named display() that prints the attribute values on the 
 Call both the methods by creating an instance std_obj of the class student
 '''
 
+from unicodedata import name
+
+
+class student:
+    def get_data(self,name,age,branch,city):
+        self.std_name = name
+        self.std_age = age
+        self.std_branch = branch
+        self.std_city = city
+    
+    def display(self):
+        print("Student name :",self.std_name)
+        print("Stutent age :",self.std_age)
+        print("Student branch :",self.std_branch)
+        print("Student city :",self.std_city)
+        
+std_obj = student()
+std_obj.get_data("saurabh",18,"B.Tech CSE","Vapi")
+std_obj.display()
