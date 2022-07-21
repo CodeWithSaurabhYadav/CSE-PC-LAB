05898832514ce4688e71acb769e6fd11b33eb97f
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2022-07-21 09:36:30 +0530

    Delete Semister__3/WebDevelopment/PHP directory

--- Semister__3/WebDevelopment/PHP/prog3.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $num = 34; // Global Varaible
-    echo "The value of num outside the function is : ".$num."\n";
-    function myFunction() {
-        echo "The value of num inside the function is : ".$num."\n";
-    }
-    myFunction();
-?>
