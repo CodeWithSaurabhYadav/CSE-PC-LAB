dfbc4c12aca027b6f52f0322ce9897fb6c9bbded
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2022-07-21 15:32:43 +0530

    Add files via upload

---
+++ Semister__3/WebDevelopment/PHP/main_programs/prog4.php
@@ -0,0 +1,20 @@
+<?php
+
+    // While Loop prime fibonaci series
+    
+    // Initilizing the variables
+    $num = 0;
+    $sum = 1;
+    $temp = 0;
+    $iterate = 0;
+
+    // Iterationg and Printing the fibonaci series
+    while ($iterate <= 1000){
+        $temp = $sum + $num;
+        $sum = $num;
+        $num = $temp;
+        echo $temp." ";
+        $iterate++;
+    }
+
+?>
