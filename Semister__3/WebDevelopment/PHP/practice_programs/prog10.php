e842e2dc48f3533bd915dd2721cd35e91c42b556
saurabh <50881320+CodeWithSaurabhYadav@users.noreply.github.com>
2022-07-22 12:49:50 +0530

    Add files via upload

---
+++ Semister__3/WebDevelopment/PHP/practice_programs/prog10.php
@@ -0,0 +1,21 @@
+<?php
+
+    // $cars = array("Volvo", "BMW", "Toyota");
+
+    // echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".\n";
+
+    // // $cars[0] = "Mercedes";
+    // $arrLength = count($cars);
+    // // $rsort($cars);
+    
+    // for ($i = 0; $i < $arrLength; $i++) {
+    //     echo $cars[$i] . " \n";
+    // }
+    // foreach ($cars as $car) {
+    //     echo $car . " \n";
+    // }
+
+    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+    echo "Peter is " . $age['Peter'] . " years old.\n";
+
+?>
