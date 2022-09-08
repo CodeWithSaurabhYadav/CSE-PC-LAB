7814fe9ae657e3f26faa326490c5cb1a86b8fe89
saurabh <sauraj.contact@gmail.com>
2022-09-08 16:52:53 +0530

    updating the previous changes commits

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from random import randint as rd
 
 
-for i in range(1,150):
+for i in range(1,500):
     for j in range(0,rd(0,7)):
         date = str(i) + 'days age'
         with open("file.txt","a") as file:
